5f6ec7d16bb1e410708f74c2d99e35f6e4291aab
nipillai8 <93091238+nipillai8@users.noreply.github.com>
2024-01-26 01:10:22 -0500

    Add files via upload

---
+++ web-projects/test-site/scripts/main.js
@@ -0,0 +1,36 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === '/Users/nidhiwork/Desktop/web-projects/test-site/images/images.jpeg') {
+    myImage.setAttribute ('src','/Users/nidhiwork/Desktop/web-projects/test-site/images/images_2.jpeg');
+  } else {
+    myImage.setAttribute ('src','/Users/nidhiwork/Desktop/web-projects/test-site/images/images.jpeg');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
